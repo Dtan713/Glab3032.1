546300bc0251465794fb1ef1c31a6184f641b8cd
Dtan713 <s.donaldtan@gmail.com>
2024-06-26 13:55:49 -0400

    adding part 3

--- src/ArithmeticOperatorsDemo.java
+++ src/ArithmeticOperatorsDemo.java
@@ -1,6 +1,6 @@
 public class ArithmeticOperatorsDemo {
-
     public static void main(String[] args) {
+
         // TODO Auto-generated method stub
         int x, y = 10, z = 5;
         x = y + z;
---
+++ src/LogicalOperatorsDemo.java
@@ -0,0 +1,13 @@
+public class LogicalOperatorsDemo {
+    public static void main(String[] args)
+    {
+Boolean x = true;
+Boolean y = false;
+System.out.println("x & y : " + (x & y));
+System.out.println("x && y : " + (x && y));
+System.out.println("x | y : " + (x | y));
+System.out.println("x || y: " + (x || y));
+System.out.println("x ^ y " + (x ^ y));
+System.out.println("!x : " + (!x));
+}
+}
